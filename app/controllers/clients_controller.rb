6716c316e636592018a9263b6257050966546f87
Grace Choi <oiograceoio@gmail.com>
2019-05-21 20:16:28 +0000

    added client controller and views

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,3 +1,40 @@
 class ClientsController < ApplicationController
+  def index
+  end
 
+  def show
+    @client = User.find_by(id: current_user)
+  end
+
+  def new
+    @client = Client.new
+  end
+
+  def create
+    @client = Client.new(client_params)
+    if @client.save
+      session[:user_id] = @client.id
+      redirect_to client_path(@client)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @client = User.find_by(id: current_user)
+  end
+
+  def update
+    @client = User.find_by(id: current_user)
+    if @client.update(client_params)
+      redirect_to cilent_path(@client)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def client_params
+    params.require(:client).permit(:name, :email, :extra, :password)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :name, :email, :password, presence: true
+  validates :email, uniqueness: true, on: :create 
 end
