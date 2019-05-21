b00dff9300487eeec3987106457d7302f614b406
Grace Choi <oiograceoio@gmail.com>
2019-05-21 22:01:17 +0000

    modified appointments controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,8 +3,9 @@ class AppointmentsController < ApplicationController
     if Artist.find_by(id: current_user)
       @artist = Artist.find_by(id: current_user)
     else
-
-
+      flash[:error] = "You must be a makeup artist to view"
+      redirect_to client_path(current_user)
+    end
   end
 
   def show
