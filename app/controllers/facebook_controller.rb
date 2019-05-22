28915f41b8f5a9011b69b7da0525e36b97052456
Grace Choi <oiograceoio@gmail.com>
2019-05-22 21:28:53 +0000

    modified omniauth

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -7,7 +7,7 @@ class FacebookController < ApplicationController
    @user = User.find_or_create_by(uid: auth['uid']) do |u|
      u.name = auth['info']['name']
      u.email = auth['info']['email']
-     u.image = auth['info']['image']
+     u.extra = auth['info']['image']
    end
 
    session[:user_id] = @user.id
