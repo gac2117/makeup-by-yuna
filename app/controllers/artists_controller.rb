17e8432098c5ff16ff6e102ccc730d9200b885c6
Grace Choi <oiograceoio@gmail.com>
2019-05-22 17:33:44 +0000

    modified artists controller and views

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,49 +1,54 @@
 class ArtistsController < ApplicationController
 
-    def index
-    end
+  def index
+  end
 
-    def show
-      @artist = Artist.find_by(id: current_user)
+  def show
+    if Client.find_by(id: current_user)
+      @client = Client.find_by(id: current_user)
     end
+    @artist = Artist.find_by(id: params[:id])
+  end
 
-    def new
-      if logged_in?
-        flash[:error] = "You are already signed in."
+  def new
+    if logged_in?
+      flash[:error] = "You are already signed in."
+      if Client.find_by(id: current_user)
+        redirect_to client_path(current_user)
+      else
         redirect_to artist_path(current_user)
-      else
-        @artist = Artist.new
       end
-    end
-
-    def create
-      @artist = Artist.new(artist_params)
-      if @artist.save
-        session[:user_id] = @artist.id
-        redirect_to artist_path(@artist)
-      else
-        flash[:error] = "Please try again."
-        render :new
-      end
-    end
-
-    def edit
-      @artist = Artist.find_by(id: current_user)
-    end
-
-    def update
-      @artist = Artist.find_by(id: current_user)
-      if @artist.update(artist_params)
-        redirect_to artist_path(@artist)
-      else
-        render :edit
-      end
-    end
-
-    private
-    def artist_params
-      params.require(:artist).permit(:name, :email, :extra, :password)
+    else
+      @artist = Artist.new
     end
   end
 
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      session[:user_id] = @artist.id
+      redirect_to artist_path(@artist)
+    else
+      flash[:error] = "Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @artist = Artist.find_by(id: current_user)
+  end
+
+  def update
+    @artist = Artist.find_by(id: current_user)
+    if @artist.update(artist_params)
+      redirect_to artist_path(@artist)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def artist_params
+    params.require(:artist).permit(:name, :email, :extra, :password)
+  end
 end
