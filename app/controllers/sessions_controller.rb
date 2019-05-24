9ceee514ad34b8b69a982c1662a0f587dacc14f4
Grace Choi <oiograceoio@gmail.com>
2019-05-23 20:09:08 -0400

    modified omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,43 +4,34 @@ class SessionsController < ApplicationController
    end
 
    def create
-     if auth
-       if @user = User.find_by(email: auth['info']['email']) 
-         session[:user_id] = @user.id
-         if Client.find_by(id: current_user)
-           redirect_to client_path(@user)
-         else
-           redirect_to artist_path(@user)
-         end
-       else
-         @user = Client.new(email: auth['info']['email']) do |u|
-           u.name = auth['info']['name']
-           u.extra = auth['info']['image']
-           u.password = SecureRandom.hex
-         end
-         if @user.save
+    if auth
+      # logs in using Facebook
+      @user = User.find_or_create_by_omniauth(auth)
+        if @user
           session[:user_id] = @user.id
-          redirect_to client_path(@user)
+          if Client.find_by(id: current_user)
+            redirect_to client_path(@user)
+          else
+            redirect_to artist_path(@user)
+          end
         else
-          raise @user.errors.full_messages.inspect
           flash[:error] = "Please try again."
           render :new
         end
-       end
-       
-     else
-       @user = User.find_by(email: params[:user][:email])
-       if @user && @user.authenticate(params[:user][:password])
-         session[:user_id] = @user.id
-         if Client.find_by(id: current_user)
-           redirect_to client_path(@user)
-         else
-           redirect_to artist_path(@user)
-         end
-       else
-         flash[:error] = "Please try again. #{@user.errors.full_messages.to_sentence}"
-         redirect_to signin_path
-       end
+    else
+      #logs in using email
+      @user = User.find_by(email: params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
+          session[:user_id] = @user.id
+            if Client.find_by(id: current_user)
+              redirect_to client_path(@user)
+            else
+              redirect_to artist_path(@user)
+            end
+        else
+          flash[:error] = "Please try again. #{@user.errors.full_messages.to_sentence}"
+          redirect_to signin_path
+        end
      end
    end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,19 @@ class User < ApplicationRecord
   validates :name, :email, :password, presence: true
   validates :email, uniqueness: true, on: :create
 
+  def self.find_or_create_by_omniauth(auth)
+	if @user = User.find_by(email: auth['info']['email']) 
+		return @user 	    
+    else
+	    @user = Client.new(email: auth['info']['email']) do |u|
+	       u.name = auth['info']['name']
+	       u.extra = auth['info']['image']
+	       u.password = SecureRandom.hex
+	    end
+	    raise "new user".inspect
+	    if @user.save
+		    return @user
+		end
+    end
+  end
 end
